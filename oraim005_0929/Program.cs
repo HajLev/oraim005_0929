197bd8c553a875288e641e24d83d438a70cd8813
Levente Hajdu <hajdu.levente@verebelyszki.hu>
2020-09-29 11:07:50 +0200

    Kész van a 2. feladat

--- oraim005_0929/Program.cs
+++ oraim005_0929/Program.cs
@@ -41,12 +41,55 @@ namespace oraim005_0929
                 Console.WriteLine("Nem lehet kiadni a kívánt összeget.");
             }
 
-            
+            Console.ReadKey();
+            Console.Clear();
+
+            //------------------------------------------------------------------------------------------------------
+
+            Console.WriteLine("2. feladat");
+            Console.WriteLine("Utazási költségtérítés");
+
+
+            Console.WriteLine("Add meg a megtett utat km-ben! ");
+            double utazas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add meg az autó fogyasztását 100 km-er literben! ");
+            double fogyasztas = Convert.ToDouble(Console.ReadLine()); ;
+            Console.WriteLine("Add meg az üzemanyag árát ft-ban! ");
+            double uzemar = Convert.ToDouble(Console.ReadLine()); 
+
+            double uzemkoltseg = (utazas * fogyasztas * uzemar) / 100;
+            if (utazas <= 100)
+            {
+                Console.WriteLine("Költségtérítés: {0} Ft.", uzemkoltseg);
+            }
+            else if (utazas > 100)
+            {
+                double koltsegter = uzemkoltseg + (utazas * 25);
+                Console.WriteLine("Költségtérítés: {0} Ft.", koltsegter);
+            }
+            else
+            {
+                Console.WriteLine("Rosz értéket adtál meg.");
+            }
 
             Console.ReadKey();
             Console.Clear();
 
+            //------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
+            Console.Clear();
+
+
+
         }
     }
 }
